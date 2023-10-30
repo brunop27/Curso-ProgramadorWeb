02459618b49322ef5124f9f8f3995b9f5dec6ea6
Brunop27 <brunopinheiro715@gmail.com>
2023-10-30 13:50:26 -0300

    Adicionado função de 'disparar' da nave aliada, foi adicionado método do Laser(), que cria o objeto laser e modificado algumas linhas para deixaar o código mais reciclável

--- Jogo_StarWars/assets/scripts/main.js
+++ Jogo_StarWars/assets/scripts/main.js
@@ -4,10 +4,12 @@ const tela = document.getElementsByTagName('body')[0];
 const game = new Game();
 //Instancia da variável da nave jogador
 let nave;
-const velocMovimento = 10;
+
+const velocMovimento = 20;
 //Instancia das variável da nave inimigo
 const maxInimigos = 10;
 const inimigos = [];
+const laser_jogador = [];
 let intervalo;
 
 //adicionando um evento á variavel “tela” com ‘keyup’ que recebe o valor do Enter quando clicado pelo usuario
@@ -17,6 +19,10 @@ tela.addEventListener('keyup', function (event){
     (game.isPause()) ? game.start() : game.pause('Pause');	
 	}else if(event.key == 'p'){
 	game.pause('Pause');
+    }
+
+    if(event.key == ' '){
+        nave.fire();
     }
 });
 
@@ -73,7 +79,7 @@ function Game(){
             inimigos.forEach(inimigo => {
                 inimigo.animation();
             })
-        }, 500);
+        }, 200);
     };
 
 	//Método que pausa e mostra mensagem
@@ -81,35 +87,19 @@ function Game(){
 	  painel.style.display = "block";
 	  placarMsg.textContent = mgs;
 	  pause = true;
+      clearInterval(intervalo);
     };	
 }
 
-//Objeto que cria a nave
-function Nave(imgNave = "wt"){
-    //Cria uma div para alocar a nave criada
-    let div = document.createElement('div');
-    //Adiciona a classe .nave na div criada acima
-    div.classList.add('nave');
-
-    //Cria uma tag img
-    let iNave = document.createElement("img");
-    
-    //Adiciona a propriedade "src" passando o caminho da imagem a ser criada
-    iNave.src = `assets/images/${imgNave}.png`;
-    
-    //Adiciona a tag img criada acima dentro da div criada anteriormente
-    div.appendChild(iNave);
-
-    //Adiciona a div na tela
-    tela.appendChild(div);
-    
+//Função criada para relacionar o objeto Nave() e objeto Laser, assim, recebendo a posição de um e outro, sem precisar instanciar um dentro do outro fugindo da POO
+function Ovni(elemento){
     //Método que captura a a largura e a altura da div da nav
-    this.w = () => div.getBoundingClientRect().width;
-    this.h = () => div.getBoundingClientRect().height;
+    this.w = () => elemento.getBoundingClientRect().width;
+    this.h = () => elemento.getBoundingClientRect().height;
     
     //Método que captura a posição da nave
-    this.x = () => div.getBoundingClientRect().x;
-    this.y = () => div.getBoundingClientRect().y;
+    this.x = () => elemento.getBoundingClientRect().x;
+    this.y = () => elemento.getBoundingClientRect().y;
 
     //Método que seta a posição inicial da nave
     this.setXY = (x, y) =>{
@@ -118,12 +108,58 @@ function Nave(imgNave = "wt"){
         }else if(x > game.w() - this.w()){
             x = game.w() - this.w();
         }
-        div.style.left = `${x}px`;
-        div.style.top = `${y}px`;  
-    }
+        elemento.style.left = `${x}px`;
+        elemento.style.top = `${y}px`;  
+    };
+}
+
+//Objeto que cria a nave
+function Nave(imgNave = "wt"){
+    //Cria uma div para alocar a nave criada
+    // let div = document.createElement('div');
+
+    //Adiciona a classe .nave na div criada acima
+    // div.classList.add('nave');
+
+    //Chama a função que recilca o codigo de criar a div
+    let div = elemento('div','nave');
+     
+    //Instancia o objeto Ovini em uma callback
+    Ovni.call(this, div);
+    
+    //Cria uma tag img
+    let iNave = document.createElement("img");
+    
+    //Adiciona a propriedade "src" passando o caminho da imagem a ser criada
+    iNave.src = `assets/images/${imgNave}.png`;
+
+    //Adiciona a tag img criada acima dentro da div criada anteriormente
+    div.appendChild(iNave);
+
+    //Adiciona a div na tela
+    // tela.appendChild(div);
+    
+    // //Método que captura a a largura e a altura da div da nav
+    // this.w = () => div.getBoundingClientRect().width;
+    // this.h = () => div.getBoundingClientRect().height;
+    
+    // //Método que captura a posição da nave
+    // this.x = () => div.getBoundingClientRect().x;
+    // this.y = () => div.getBoundingClientRect().y;
+
+    // //Método que seta a posição inicial da nave
+    // this.setXY = (x, y) =>{
+    //     if(x<0){
+    //         x = 0;
+    //     }else if(x > game.w() - this.w()){
+    //         x = game.w() - this.w();
+    //     }
+    //     div.style.left = `${x}px`;
+    //     div.style.top = `${y}px`;  
+    // }
 
     //Método que calcula o posicionamento da nave referente a tela
-    let posicaoInicial = () =>{
+    let posicaoInicial = () => {
         this.setXY(
             game.w()/2 - this.w()/2, //valor de X
             game.h() - this.h() - 10
@@ -132,8 +168,18 @@ function Nave(imgNave = "wt"){
 
     //Carrega a imagem com a posição setada
     iNave.onload = posicaoInicial;
+    
     //Toda vez que chamar a função onload, esta, receberá uma nova função que será a “fn”, que recebe o i.onload;
     this.onload = (fn) => iNave.onload = fn;
+    
+    //Metodo que cria o laser
+    this.fire = () => {
+        let laser = new Laser();
+        let x = this.x() + this.w()/2 - laser.w()/2;
+        let y = this.y() - laser.h() - 1;
+        laser.setXY(x, y);
+        laser_jogador.push(laser);
+    }
 }
 
 //Instancia o objeto Nave(), herdando todas as características para criar o Inimigo
@@ -157,7 +203,24 @@ function Inimigos(imagem = 'cp1'){
             this.setPosicaoInicial();
         };
     }
-
+  
     //Função(herdada), acionada quando inicia o game
     this.onload(this.setPosicaoInicial);
+}
+
+//Função usada para reciclar códigos repetidos. A mesma, substitui a linhas que criam as div e adicionam nas telas
+function elemento(tag, classe){
+    //Cria um elemento baseado na tag passada por parametro
+	let elemento = document.createElement(tag);
+    //Adiciona a classe na div criada acima
+	elemento.classList.add(classe);
+    //Adiciona a tag na tela
+	tela.appendChild(elemento);
+	return elemento;
+}
+
+//Função usada para criar o laser da nave do jogador
+function Laser(){
+    let div = elemento('div','laser');
+    Ovni.call(this, div);
 }
